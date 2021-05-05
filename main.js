d7f996f42ef812b16fb453f18b105d5cdc9bbb64
Lelkes István <47304380+ilelkes@users.noreply.github.com>
2021-05-05 23:11:16 +0200

    Lesson 4

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inventory: 100,
+            // solution
+            onSale: true
+            // solution
+        }
+    }
+})
